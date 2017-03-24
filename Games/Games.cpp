ef1ad5c37a1e70948dda58497e289018fbc00f4e
LukaMatkic <matkic.luka@gmail.com>
2017-03-24 14:15:29 +0100

    Created main .cpp file

---
+++ Games/Games.cpp
@@ -0,0 +1,22 @@
+// Games.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+void showMenu();
+
+int main()
+{
+	cout << "KLIKNI ZA IZLAZ IZ PROGRAMA..." << endl;
+	system("pause>nul");
+    return 0;
+}
+
+void showMenu()
+{
+	cout << "MENU: " << endl << endl;
+	cout << "\t0\tIZLAZ" << endl;
+}
